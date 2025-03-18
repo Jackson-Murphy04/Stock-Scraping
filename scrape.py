b9989397903f3cc2406cdb9788d982a6e2171da6
Jackson Murphy <jackmurphy687@gmail.com>
2025-03-18 15:35:18 -0400

    basic scrape

--- scrape.py
+++ scrape.py
@@ -1,12 +1,42 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
 # set driver path for chromeDriver
 service = Service(executable_path="chromedriver.exe")
-driver = webdriver.Chrome(service=service)
 
-# set target site
-driver.get("https://google.com")
+# Configure Chrome options
+options = Options()
+options.add_argument("--headless")  # Enable headless mode
 
-# always quit driver
+# init chrome driver
+driver = webdriver.Chrome(options=options, service=service)
+
+# make array of target sites 
+sites = ["https://finance.yahoo.com/markets/stocks/trending/", 
+         "https://finance.yahoo.com/markets/stocks/gainers/", 
+         "https://finance.yahoo.com/markets/stocks/most-active/",
+         "https://finance.yahoo.com/topic/stock-market-news/",
+         "https://www.marketwatch.com/",
+         "https://www.marketwatch.com/column/need-to-know?mod=newsviewer_click",
+         "https://seekingalpha.com/editors-picks",
+         "https://seekingalpha.com/market-news/trending",
+         "https://www.bloomberg.com/markets",
+         "https://www.reddit.com/r/wallstreetbets/",
+         "https://www.reddit.com/r/stocks/",
+         "https://www.reddit.com/r/investing/",
+         "https://finviz.com/",
+         "https://finviz.com/news.ashx",
+         "https://finviz.com/screener.ashx?v=120&s=ta_topgainers"
+        ]
+
+# loop array and open each site
+for site in sites:
+  # set target site
+  driver.get(site)
+  # output site HTML
+  print(driver.page_source)
+
+# close site
 driver.quit()
+
