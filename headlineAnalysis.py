0a5f1deff49b40faf77fe3e03325a921a11e250c
PhilipVickery <pvickery@vols.utk.edu>
2025-04-29 14:05:20 -0400

    sorting and output
    Made a new list that stores headlines and their scores, then sorted the list, and outputs to a scores file

--- headlineAnalysis.py
+++ headlineAnalysis.py
@@ -20,3 +20,15 @@ scores = []
 for headline in headlines:
     score = sia.polarity_scores(headline)
     scores.append(score['compound'])
+
+# create list combining headlines and scores
+combine = list(zip(headlines, scores))
+
+# sort list from highest to lowest score
+sort_combine = sorted(combine, key = lamda x: x[1], reverse=True)
+
+# output headlines and scores
+with open("outputScores.txt", "a") as file:
+        for headline, score in sort_combine:
+                file.write(f"{headline}: {score}\n")
+               
